7d4e660b48896445dde7f71b7d021130c549a2e8
@max.naumanen <max.naumanen@tbwa.fi>
2024-11-14 15:18:27 +0200

    commit coz its good for u

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,15 +3,15 @@ import { defineCollection, z } from "astro:content";
 const main = defineCollection({
 	type: "content",
 	schema: z.object({
-		s1heading: z.string(),
-		s1display: z.string(),
-    s1subtitle: z.string(),
-    s2trailerheading: z.string(),
-    s2fulllengthheading: z.string(),
+		s1heading: z.string().optional(),
+		s1display: z.string().optional(),
+    s1subtitle: z.string().optional(),
+    s2trailerheading: z.string().optional(),
+    s2fulllengthheading: z.string().optional(),
 
-    s3heading: z.string(),
-    s3body1: z.string(),
-    s3body2: z.string(),
+    s3heading: z.string().optional(),
+    s3body1: z.string().optional(),
+    s3body2: z.string().optional(),
 
     s4heading: z.string().optional(),
     s4body1: z.string().optional(),
@@ -25,13 +25,13 @@ const main = defineCollection({
     s6body1: z.string().optional(),
     s6body2: z.string().optional(),
 
-    s7heading: z.string(),
-    s7subheading: z.string(),
-    s7body1: z.string(),
-    s7body2: z.string(),
+    s7heading: z.string().optional(),
+    s7subheading: z.string().optional(),
+    s7body1: z.string().optional(),
+    s7body2: z.string().optional(),
 	}),
 });
 
 export const collections = {
-	'main': main,
+	main,
 };
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      sans: ["Open Sans", "sans-serif"],
+      display: ["Kurdis", "sans-serif"],
+    },
+    extend: {
+      fontSize: {
+        "10xl": "16rem",
+      },
+      fontFamily: {
+        condensed: ["Kurdis Condensed", "sans-serif"],
+      },
+      colors: {
+        ivory: "#d5d0ca",
+        blue: "#3285da",
+      },
+    },
+  },
+  plugins: [],
+};
