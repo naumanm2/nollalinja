523cd4d834cb56613d33b3774746d4dbb452b98b
@max.naumanen <max.naumanen@tbwa.fi>
2024-11-15 15:28:56 +0200

    condensed font still dont work

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: [
+    "./public/**/*.html",
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  ],
   theme: {
     fontFamily: {
       sans: ["Open Sans", "sans-serif"],
     },
     extend: {
       fontFamily: {
-        display: ["Kurdis", "sans-serif"],
         condensed: ["KurdisCondensed", "sans-serif"],
+        display: ["Kurdis", "sans-serif"],
       },
       keyframes: {
         fade: {
